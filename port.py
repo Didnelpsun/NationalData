b6c89b3b0fd8be10768655f4bd7cb44824c7a730
Didnelpsun <2675350965@qq.com>
2021-07-12 23:20:49 +0800

    Update port.py

--- port.py
+++ port.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     data_url = "https://data.stats.gov.cn/easyquery.htm?m=QueryData&dbcode=hgnd&rowcode=zb&colcode=sj&wds=%5B%5D&dfwds=%5B%7B%22wdcode%22%3A%22sj%22%2C%22valuecode%22%3A%22LAST20%22%7D%5D&k1=1625042754510"
     show_data(base_url, data_url, '快递量')
     show_data(base_url, data_url, '快递业务收入')
+    
